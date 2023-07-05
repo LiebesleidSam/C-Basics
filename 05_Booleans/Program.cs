2b7d8a0ed19c944ea525ed5c23048b5f3c181645
Sam Ruiz <samuel.ruiz.mtg@gmail.com>
2023-07-05 14:19:06 -0400

    Booleans project added

---
+++ 05_Booleans/Program.cs
@@ -0,0 +1,32 @@
+﻿// NOT Operator
+// AND Operator
+// OR Operator
+// XOR Operator
+
+// Use the bool keyword
+bool csharpIsCool = false;
+csharpIsCool = true;
+
+// Boolean Logical Operators
+// NOT Operator
+bool isTrue = !false;
+bool isFalse = !true;
+
+// AND Operator
+bool trueAndExample = true && true;
+bool falseAndExample = true && false;
+
+// OR Operator
+bool trueOrExample = true || false;
+bool falseOrExample = false || false;
+
+// XOR Operator
+bool trueExclusiveOr = true ^ false;
+bool falseExclusiveOr = true ^ true;
+
+bool isRaining = true;
+bool isGoingOutside = true;
+
+if (isRaining && isGoingOutside) {
+    Console.WriteLine("Bring an Umbrella!");
+}
