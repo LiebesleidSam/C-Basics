413517979020386305a70c405d5df2f807694133
Sam Ruiz <samuel.ruiz.mtg@gmail.com>
2023-07-05 12:29:47 -0400

    Strings project added

---
+++ 04_Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string firstName = "Sam";
+string lastName = "Ruiz";
+
+// Concatenation
+string concatenatedString = firstName + " " + lastName;
+
+// Composite formatting
+string compositeString = string.Format("My name is {0} {1}.", firstName, lastName);
+
+// String Interpolation
+string interpolatedString = $"My name is still {firstName} {lastName}";
+
+
+string cardName = "Black Lotus";
+string storeName = "Game Time";
+
+string interpolatedSentence = $"{firstName} {lastName} works at {storeName} and sells cards like {cardName}.";
+Console.WriteLine(interpolatedSentence);
+
+string compositeSentence = string.Format("{0} {1} works at {2} and sells cards like {3}.", firstName, lastName, storeName, cardName);
+Console.WriteLine(compositeSentence);
+
+
