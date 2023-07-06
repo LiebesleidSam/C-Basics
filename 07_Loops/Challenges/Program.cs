38a0f4b91bd27cc52e5e4daca24fd4bd75b7a691
Sam Ruiz <samuel.ruiz.mtg@gmail.com>
2023-07-06 14:56:47 -0400

    Loops Challenges added

---
+++ 07_Loops/Challenges/Program.cs
@@ -0,0 +1,32 @@
+﻿// Bronze
+for (int i = 500; i <= 525; i++)
+{
+    Console.WriteLine(i);
+}
+
+// Silver
+for (int i = 0; i <= 100; i += 5)
+{
+    Console.WriteLine(i);
+}
+
+// Gold
+for (int i = 1; i <=100; i++)
+{
+    if (i % 15 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
