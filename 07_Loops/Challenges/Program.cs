820a60620f5d5a7f4fabb80914fb80ad7ae07453
Sam Ruiz <samuel.ruiz.mtg@gmail.com>
2023-07-06 15:12:17 -0400

    Loops Challenges changed

--- 07_Loops/Challenges/Program.cs
+++ 07_Loops/Challenges/Program.cs
@@ -13,7 +13,11 @@ for (int i = 0; i <= 100; i += 5)
 // Gold
 for (int i = 1; i <=100; i++)
 {
-    if (i % 15 == 0)
+    if (i % 4 == 0)
+    {
+        continue;
+    }
+    else if (i % 15 == 0)
     {
         Console.WriteLine("FizzBuzz");
     }
@@ -24,6 +28,10 @@ for (int i = 1; i <=100; i++)
     else if (i % 5 == 0)
     {
         Console.WriteLine("Buzz");
+    }
+    else if (i % 7 == 0)
+    {
+        continue;
     }
     else
     {
