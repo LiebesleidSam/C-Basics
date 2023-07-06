95ce3c2edd344b24480b0d197f341ad1c2d6894d
Sam Ruiz <samuel.ruiz.mtg@gmail.com>
2023-07-06 10:57:53 -0400

    Ternaries added

--- 06_Conditionals/IfStatements/Program.cs
+++ 06_Conditionals/IfStatements/Program.cs
@@ -20,3 +20,31 @@ if (isRaining && isGoingOutside)
 {
     Console.WriteLine("Bring an umbrella!");
 }
+
+Console.WriteLine("How are you doing today on a scale from 1-5?");
+string? dailyMood = Console.ReadLine();
+
+if (dailyMood == "5")
+{
+    Console.WriteLine("Sounds like you're doing awesome!");
+}
+else if (dailyMood == "4")
+{
+    Console.WriteLine("That's pretty good!");
+}
+else if (dailyMood == "3")
+{
+    Console.WriteLine("Could be better, could be worse.");
+}
+else if (dailyMood == "2")
+{
+    Console.WriteLine("I'm sorry to hear that.");
+}
+else if (dailyMood == "1")
+{
+    Console.WriteLine("Oh dear, I hope tomorrow is better.");
+}
+else
+{
+    Console.WriteLine("I'm not sure what you meant by that.");
+}
---
+++ 06_Conditionals/Ternaries/Program.cs
@@ -0,0 +1,8 @@
+﻿// Ternaries syntax
+// boolean expression ? expression if true : expression if false;
+
+Console.WriteLine("Enter your age: ");
+string? response = Console.ReadLine();
+int age = int.Parse(response);
+
+string output = age >= 18 ? "You can vote!" : "You're too young to vote.";
