631d02d197acbe8882b2e35e5873b69844af7874
Sam Ruiz <samuel.ruiz.mtg@gmail.com>
2023-07-10 10:25:02 -0400

    Classes added

---
+++ 08_Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using Classes;
+
+// type name = new typeConstructor
+Donut tasty = new Donut("Cream Filled");
+
+// tasty.donutType = "Jelly Filled";
+
+Console.WriteLine(tasty.donutType);
+
+namespace Classes
+{
+    class Donut
+    {
+        // Field
+        public string donutType;
+
+        // Constructor
+        // public Donut() {}
+        public Donut(string type)
+        {
+            // Assinging the constructor parameter to the field
+            // Whatever is passed into the constructor it passes its value to the field
+            donutType = type;
+        }
+    }
+
+}
