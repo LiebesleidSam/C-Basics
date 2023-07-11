615745d1204fe085a543468870c956ed78a55de7
Sam Ruiz <samuel.ruiz.mtg@gmail.com>
2023-07-11 15:59:00 -0400

    SuperChallenge added

---
+++ Challenges/CalculatorClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using Classes;
+
+Calculator calc = new Calculator("decimal");
+Console.WriteLine(calc.Add(3.14, 4.2));
+
+namespace Classes
+{
+    class Calculator
+    {
+        public string AnswerType {get; set;}
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public Calculator(string type)
+        {
+            this.AnswerType = type;
+        }
+    }
+}
---
+++ Challenges/SuperChallenge/Program.cs
@@ -0,0 +1,15 @@
+﻿string longWord = "Supercalifragilisticexpialidocious";
+
+foreach (char letter in longWord)
+{
+    if (letter == 'i')
+    {
+        Console.WriteLine(letter);
+    }
+    else
+    {
+        Console.WriteLine("Not an i");
+    }
+}
+
+Console.WriteLine(longWord.Length);
