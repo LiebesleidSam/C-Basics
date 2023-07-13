8c09df812be8169c8e64706d6fd5c7a72a7231fa
Sam Ruiz <samuel.ruiz.mtg@gmail.com>
2023-07-13 09:22:24 -0400

    Bird game finished

--- DrofsnarGame/Program.cs
+++ DrofsnarGame/Program.cs
@@ -7,3 +7,15 @@ var line = sr.ReadLine();
 string[] words = line.Split(",");
 
 Drofsnar player = new Drofsnar(5000, 3);
+
+foreach (string word in words)
+{
+    player.EncounterObject(word);
+    if (player.Lives == 0)
+    {
+        break;
+    }
+}
+
+Console.WriteLine($"Final Points: {player.Points}");
+Console.WriteLine($"Final Lives: {player.Lives}");
